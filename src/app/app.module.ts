9d39f727b02e69f2904167cafb925ac82960864f
Anthony <anthony@modularity.io>
2018-06-12 15:51:08 +0100

    added user sign on form firebaseAuth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, isDevMode } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './shared/angular-material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule} from './app-routing.module';
-import { RouterLink } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { HomeComponent } from './features/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgReduxModule, NgRedux, DevToolsExtension } from '@angular-redux/store';
 import { IAppState, rootReducer, INITIAL_STATE } from './store/store';
+import { environment } from '../environments/environment';
 
 
 
@@ -28,13 +30,15 @@ import { IAppState, rootReducer, INITIAL_STATE } from './store/store';
     AngularMaterialModule,
     BrowserModule,
     BrowserAnimationsModule,
+    AngularFireModule,
     FormsModule,
     ReactiveFormsModule,
     FlexLayoutModule,
     HttpClientModule,
-    NgReduxModule
+    NgReduxModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   
-
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDbfXUHZQwPDkmrSXcJ6cMlqsT0MBrrkVA",
+    authDomain: "inhaus-7850b.firebaseapp.com",
+    databaseURL: "https://inhaus-7850b.firebaseio.com",
+    projectId: "inhaus-7850b",
+    storageBucket: "inhaus-7850b.appspot.com",
+    messagingSenderId: "431010218412"
+  }
 };
