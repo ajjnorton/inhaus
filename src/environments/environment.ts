288eab00ac6d3e7c07019de699532e95808340a7
Anthony Norton <anthony@modularity.io>
2018-08-15 15:49:27 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,7 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyDbfXUHZQwPDkmrSXcJ6cMlqsT0MBrrkVA",
-    authDomain: "inhaus-7850b.firebaseapp.com",
-    databaseURL: "https://inhaus-7850b.firebaseio.com",
-    projectId: "inhaus-7850b",
-    storageBucket: "inhaus-7850b.appspot.com",
-    messagingSenderId: "431010218412"
-  }
-};
+
 
 /*
  * In development mode, to ignore zone related error stack frames such as
