7ce32aaa3262d0b90c0902212907d003842ea97f
Anthony Norton <anthony@modularity.io>
2018-08-15 15:49:54 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyDbfXUHZQwPDkmrSXcJ6cMlqsT0MBrrkVA",
-    authDomain: "inhaus-7850b.firebaseapp.com",
-    databaseURL: "https://inhaus-7850b.firebaseio.com",
-    projectId: "inhaus-7850b",
-    storageBucket: "inhaus-7850b.appspot.com",
-    messagingSenderId: "431010218412"
-  }
+ 
 };
